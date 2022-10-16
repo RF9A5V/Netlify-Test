ea63daff9912e81f503bffd5cd1bea68e9c6199e
Steven Zhang <steven@pursuit.org>
2022-10-16 15:36:23 -0400

    Use env for API key

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import './App.css';
 import { useState } from "react";
 
 function App() {
-
   const [drinks, setDrinks] = useState([]);
 
   function getDrinksOnClick() {
     fetch("https://the-cocktail-db.p.rapidapi.com/popular.php", {
       method: "GET",
       headers: {
-        'X-RapidAPI-Key': '<MY_SECRET_API_KEY>',
+        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
         'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
       }
     }).then(response => response.json()).then(data => {
